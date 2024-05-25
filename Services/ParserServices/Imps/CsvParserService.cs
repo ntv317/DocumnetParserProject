9bfadaa01c9d8a7d0397872a15033c883740c6e5
Van <ntv.31792@gmail.com>
2024-05-25 11:32:37 +0700

    implemented parser services

--- Services/ParserServices/Imps/CsvParserService.cs
+++ Services/ParserServices/Imps/CsvParserService.cs
@@ -1,15 +1,57 @@
 using Database.Models;
 using Database.Models.Enums;
 using Microsoft.AspNetCore.Http;
+using Microsoft.VisualBasic.FileIO;
+using Services.Common;
+using Services.Common.Interfaces;
 using Services.ParserServices.Interfaces;
 
 namespace Services.ParserServices.Imps;
 
 public class CsvParserService:IParserService
 {
+    private readonly IValidateValueService _validateValueService;
+
+    public CsvParserService(IValidateValueService validateValueService)
+    {
+        _validateValueService = validateValueService;
+    }
     public async Task<ICollection<Operation>> Parse(IFormFile file)
     {
-        throw new NotImplementedException();
+        using (var parser = new TextFieldParser(file.OpenReadStream()) )
+        {
+            parser.HasFieldsEnclosedInQuotes = true;
+            parser.SetDelimiters(",");
+            var transactions = new List<Operation>();
+
+            while (!parser.EndOfData)
+            {
+                var fields = parser.ReadFields();
+                if (fields != null && fields.Any(x => string.IsNullOrEmpty(x)))
+                {
+                    throw new Exception($"This row contain null. {fields}");
+                }
+                var id = fields[0];
+                var amount = fields[1].ParseToDecimal();
+                if (!_validateValueService.ValidateCurrency(fields[2]))
+                {
+                    throw new Exception($"This raw contain not valid currency. {fields}");
+                }
+                var currency = fields[2].ToUpper();
+                var date = fields[3].ParseDate("dd/MM/yyyy hh:mm:ss");
+                var status = ConvertStatus(fields[4]);
+                transactions.Add(new Operation()
+                {
+                    Id = id,
+                    CurrencyCode = currency,
+                    Amount = amount,
+                    Date = date,
+                    Status = status
+                });
+            } 
+                
+            return await Task.FromResult<ICollection<Operation>>(transactions);
+        }
     }
 
     public OperationStatus ConvertStatus(string status)
--- Services/ParserServices/Imps/XmlParserService.cs
+++ Services/ParserServices/Imps/XmlParserService.cs
@@ -1,15 +1,72 @@
+using System.Xml.Serialization;
 using Database.Models;
 using Database.Models.Enums;
 using Microsoft.AspNetCore.Http;
+using Services.Common;
+using Services.Common.Interfaces;
 using Services.ParserServices.Interfaces;
 
 namespace Services.ParserServices.Imps;
 
 public class XmlParserService:IParserService
 {
+    private readonly IValidateValueService _validateValueService;
+
+    public XmlParserService(IValidateValueService validateValueService)
+    {
+        _validateValueService = validateValueService;
+    }
     public async Task<ICollection<Operation>> Parse(IFormFile file)
     {
-        throw new NotImplementedException();
+        using (var stream = file.OpenReadStream())
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Transactions));
+            var result = (Transactions) serializer.Deserialize(stream)!;
+            if (result == null)
+            {
+                throw new Exception("No data");
+            }
+            var listTransaction = new List<Operation>();
+            foreach (var res in result.XmlTransactions)
+            {
+                if (string.IsNullOrEmpty(res.Id))
+                {
+                    throw new Exception($"Id is null");
+                }
+                    
+                if (string.IsNullOrEmpty(res.TransactionDate))
+                {
+                    throw new Exception($"TransactionDate is null");
+                }
+                var date = res.TransactionDate.ParseDate( "yyyy-MM-ddTHH:mm:ss");
+                    
+                if (string.IsNullOrEmpty(res.PaymentDetails.CurrencyCode))
+                {
+                    throw new Exception($"CurrencyCode is null");
+                }
+                if (!_validateValueService.ValidateCurrency(res.PaymentDetails.CurrencyCode))
+                {
+                    throw new Exception("Currency format is not correct");
+                }
+                    
+                if (string.IsNullOrEmpty(res.Status))
+                {
+                    throw new Exception($"Status is null");
+                }
+                var status = ConvertStatus(res.Status);
+                    
+                listTransaction.Add(new Operation()
+                {
+                    Id = res.Id,
+                    Amount = res.PaymentDetails.Amount,
+                    CurrencyCode = res.PaymentDetails.CurrencyCode,
+                    Date = date,
+                    Status = status
+                });
+            }
+
+            return await Task.FromResult<ICollection<Operation>>(listTransaction);
+        }
     }
 
     public OperationStatus ConvertStatus(string status)
@@ -27,5 +84,34 @@ public class XmlParserService:IParserService
 
             
         }
+    }
+    
+    [XmlRoot(ElementName="PaymentDetails")]
+    public class PaymentDetails {
+        [XmlElement(ElementName="Amount")]
+        public decimal Amount { get; set; }
+        [XmlElement(ElementName="CurrencyCode")]
+        public string CurrencyCode { get; set; }
+    }
+
+    [XmlRoot(ElementName="Transaction")]
+    public class XmlTransaction {
+        [XmlElement(ElementName="TransactionDate")]
+        public string TransactionDate { get; set; }
+        
+        [XmlElement(ElementName="PaymentDetails")]
+        public PaymentDetails PaymentDetails { get; set; }
+        
+        [XmlElement(ElementName="Status")]
+        public string Status { get; set; }
+        
+        [XmlAttribute(AttributeName="id")]
+        public string Id { get; set; }
+    }
+
+    [XmlRoot(ElementName="Transactions")]
+    public class Transactions {
+        [XmlElement(ElementName="Transaction")]
+        public List<XmlTransaction> XmlTransactions { get; set; }
     }
 }
